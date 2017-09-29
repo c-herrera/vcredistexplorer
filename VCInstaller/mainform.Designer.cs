282acacd2d6cf1aade354da8fb9f6e6324fa62a2
C. Herrera <pro.cah@gmail.com>
2017-09-28 22:59:28 -0500

    Initial upload
    Tool helps to install vcredist packages

---
+++ VCInstaller/mainform.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace VCInstaller
+{
+    partial class frm_installer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_installer));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chk_file_list = new System.Windows.Forms.CheckedListBox();
+            this.btn_install = new System.Windows.Forms.Button();
+            this.grp_options = new System.Windows.Forms.GroupBox();
+            this.radio_select_none = new System.Windows.Forms.RadioButton();
+            this.radio_select_all = new System.Windows.Forms.RadioButton();
+            this.lbl_details = new System.Windows.Forms.Label();
+            this.btn_search_show = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txt_info = new System.Windows.Forms.TextBox();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.grp_options.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDown);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(498, 297);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chk_file_list);
+            this.tabPage1.Controls.Add(this.btn_install);
+            this.tabPage1.Controls.Add(this.grp_options);
+            this.tabPage1.Controls.Add(this.lbl_details);
+            this.tabPage1.Controls.Add(this.btn_search_show);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(490, 271);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chk_file_list
+            // 
+            this.chk_file_list.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.chk_file_list.CheckOnClick = true;
+            this.chk_file_list.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chk_file_list.Font = new System.Drawing.Font("Intel Clear", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_file_list.FormattingEnabled = true;
+            this.chk_file_list.Location = new System.Drawing.Point(8, 6);
+            this.chk_file_list.Name = "chk_file_list";
+            this.chk_file_list.Size = new System.Drawing.Size(318, 206);
+            this.chk_file_list.TabIndex = 10;
+            // 
+            // btn_install
+            // 
+            this.btn_install.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_install.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_install.Image = ((System.Drawing.Image)(resources.GetObject("btn_install.Image")));
+            this.btn_install.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_install.Location = new System.Drawing.Point(198, 230);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(128, 23);
+            this.btn_install.TabIndex = 9;
+            this.btn_install.Text = "Start Install";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // grp_options
+            // 
+            this.grp_options.Controls.Add(this.radio_select_none);
+            this.grp_options.Controls.Add(this.radio_select_all);
+            this.grp_options.Font = new System.Drawing.Font("Intel Clear", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grp_options.Location = new System.Drawing.Point(351, 82);
+            this.grp_options.Name = "grp_options";
+            this.grp_options.Size = new System.Drawing.Size(130, 90);
+            this.grp_options.TabIndex = 8;
+            this.grp_options.TabStop = false;
+            this.grp_options.Text = "Options";
+            // 
+            // radio_select_none
+            // 
+            this.radio_select_none.AutoSize = true;
+            this.radio_select_none.Location = new System.Drawing.Point(21, 56);
+            this.radio_select_none.Name = "radio_select_none";
+            this.radio_select_none.Size = new System.Drawing.Size(87, 23);
+            this.radio_select_none.TabIndex = 1;
+            this.radio_select_none.TabStop = true;
+            this.radio_select_none.Text = "Select None";
+            this.radio_select_none.UseVisualStyleBackColor = true;
+            this.radio_select_none.Click += new System.EventHandler(this.radio_select_none_CheckedChanged);
+            // 
+            // radio_select_all
+            // 
+            this.radio_select_all.AutoSize = true;
+            this.radio_select_all.Location = new System.Drawing.Point(21, 33);
+            this.radio_select_all.Name = "radio_select_all";
+            this.radio_select_all.Size = new System.Drawing.Size(74, 23);
+            this.radio_select_all.TabIndex = 0;
+            this.radio_select_all.TabStop = true;
+            this.radio_select_all.Text = "Select All";
+            this.radio_select_all.UseVisualStyleBackColor = true;
+            this.radio_select_all.Click += new System.EventHandler(this.radio_select_all_CheckedChanged);
+            // 
+            // lbl_details
+            // 
+            this.lbl_details.AutoSize = true;
+            this.lbl_details.Font = new System.Drawing.Font("Intel Clear", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_details.Location = new System.Drawing.Point(348, 54);
+            this.lbl_details.Name = "lbl_details";
+            this.lbl_details.Size = new System.Drawing.Size(45, 19);
+            this.lbl_details.TabIndex = 6;
+            this.lbl_details.Text = "Found :";
+            // 
+            // btn_search_show
+            // 
+            this.btn_search_show.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_search_show.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_search_show.Image = ((System.Drawing.Image)(resources.GetObject("btn_search_show.Image")));
+            this.btn_search_show.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_search_show.Location = new System.Drawing.Point(351, 6);
+            this.btn_search_show.Name = "btn_search_show";
+            this.btn_search_show.Size = new System.Drawing.Size(130, 23);
+            this.btn_search_show.TabIndex = 5;
+            this.btn_search_show.Text = "Show Files";
+            this.btn_search_show.UseVisualStyleBackColor = true;
+            this.btn_search_show.Click += new System.EventHandler(this.btn_search_show_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txt_info);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(490, 271);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txt_info
+            // 
+            this.txt_info.BackColor = System.Drawing.Color.LightYellow;
+            this.txt_info.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_info.Location = new System.Drawing.Point(61, 25);
+            this.txt_info.Multiline = true;
+            this.txt_info.Name = "txt_info";
+            this.txt_info.ReadOnly = true;
+            this.txt_info.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_info.Size = new System.Drawing.Size(423, 231);
+            this.txt_info.TabIndex = 0;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Image = ((System.Drawing.Image)(resources.GetObject("btn_exit.Image")));
+            this.btn_exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_exit.Location = new System.Drawing.Point(369, 305);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(130, 23);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // frm_installer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 341);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btn_exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_installer";
+            this.Text = "VC Installer";
+            this.Load += new System.EventHandler(this.frm_installer_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.grp_options.ResumeLayout(false);
+            this.grp_options.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.CheckedListBox chk_file_list;
+        private System.Windows.Forms.Button btn_install;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.GroupBox grp_options;
+        private System.Windows.Forms.RadioButton radio_select_none;
+        private System.Windows.Forms.RadioButton radio_select_all;
+        private System.Windows.Forms.Label lbl_details;
+        private System.Windows.Forms.Button btn_search_show;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txt_info;
+    }
+}
+
---
+++ VCInstaller/mainform.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VCInstaller
+{
+    public partial class frm_installer : Form
+    {
+        string[] installer_files_full_path;
+        string[] installer_files;
+        string[] selected_files;
+
+        string arguments2005;
+        string arguments2008;
+        string arguments2010;
+        string arguments2012;
+        string arguments2013;
+        string arguments2015;
+
+        string log;
+        string info;
+        public frm_installer()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_installer_Load(object sender, EventArgs e)
+        {
+            if ( WindowsIdentity.GetCurrent().Owner == WindowsIdentity.GetCurrent().User )   // Check for Admin privileges   
+            {
+                try
+                {
+                    this.Visible = false;
+                    ProcessStartInfo info = new ProcessStartInfo(Application.ExecutablePath); // my own .exe
+                    info.UseShellExecute = true;
+                    info.Verb = "runas";   // invoke UAC prompt
+                    Process.Start(info);
+                }
+                catch ( Win32Exception ex )
+                {
+                    if ( ex.NativeErrorCode == 1223 ) //The operation was canceled by the user.
+                    {
+                        MessageBox.Show("Why did you not selected Yes?", "WHY?", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        Application.Exit();
+                    }
+                    else
+                        throw new Exception("Something went wrong :-(");
+                }
+                Application.Exit();
+            }
+            else
+            {
+                //    MessageBox.Show("I have admin privileges :-)");
+            }
+
+            radio_select_all.Enabled = false;
+            radio_select_none.Enabled = false;
+            btn_install.Enabled = false;
+
+            arguments2005 = "/q";
+            arguments2008 = "/qb";
+            arguments2010 = "/passive /norestart";
+            arguments2012 = "/passive /norestart";
+            arguments2013 = "/install /passive /norestart";
+            arguments2015 = "/install /passive /norestart";            
+
+            tabControl1.SelectedIndex = 0;           
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "VC INSTALLER"; 
+            notifyIcon1.Icon = this.Icon;
+
+            log = string.Empty;
+
+            info = string.Empty;
+
+            info += "VCINstaller v" + Application.ProductVersion + Environment.NewLine;
+            info += "How to use this tool :" + Environment.NewLine;
+            info += "1 . Copy all your vcredist packages into a folder, and copy this tool too." + Environment.NewLine;
+            info += "2 . Click on the 'Show Files' Button and select your packages" + Environment.NewLine;
+            info += "3 . Once you are sure of your selection click the 'Start Install' button" + Environment.NewLine;
+            info += "4 . Wait for the packages to finish, a notifycation will be seen when the process is done" + Environment.NewLine;
+            info += " " + Environment.NewLine;
+            info += "This tool works better if your vcredist are named in a more readable manner such as :" + Environment.NewLine;
+            info += "vcredist-2010-x64.exe, vcredist_x86-2008.exe, etc" + Environment.NewLine;
+            info += "Just make sure the name vcredist +<year>+<type> is on the filenaame." + Environment.NewLine;
+            info += "Tool developer : C. A. Herrera" + Environment.NewLine;
+
+            txt_info.Text = info;
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_search_show_Click(object sender, EventArgs e)
+        {
+            installer_files_full_path = Directory.GetFiles(Environment.CurrentDirectory, "*.exe", SearchOption.TopDirectoryOnly);
+            installer_files = new string[installer_files_full_path.Length];
+
+            chk_file_list.Items.Clear();            
+
+            for ( int i = 0 ; i < installer_files_full_path.Length ; i++ )
+            {
+                installer_files[i] = Path.GetFileName(installer_files_full_path[i]);
+            }
+
+
+            for (int i = 0 ; i < installer_files_full_path.Length ; i++ )
+            {
+                chk_file_list.Items.Add(installer_files[i]);
+            }
+
+            lbl_details.Text += " " + installer_files_full_path.Length;
+
+            btn_install.Enabled = true;
+            radio_select_all.Enabled = true;
+            radio_select_none.Enabled = true;       
+        }
+
+        private void radio_select_all_CheckedChanged(object sender, EventArgs e)
+        {
+            for ( int i = 0 ; i < chk_file_list.Items.Count; i++)
+            {
+                chk_file_list.SetItemCheckState(i,CheckState.Checked);
+            }
+        }
+
+        private void radio_select_none_CheckedChanged(object sender, EventArgs e)
+        {
+            for ( int i = 0 ; i < chk_file_list.Items.Count ; i++ )
+            {
+                chk_file_list.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            int inner = 0;
+            selected_files = new string[chk_file_list.Items.Count];
+
+            for ( int i = 0 ; i < chk_file_list.Items.Count ; i++ )
+            {
+                if ( chk_file_list.GetItemChecked(i) == true )
+                {
+                    selected_files[inner++] = chk_file_list.Items[i].ToString();
+                }
+            }
+
+            log += "Process started at " + DateTime.Now.TimeOfDay + Environment.NewLine;
+            log += "System version reported is : " + Environment.OSVersion + Environment.NewLine;
+            log += "Trying the installation of " + inner + " packages" + Environment.NewLine;
+
+
+            for ( int i = 0 ; i < chk_file_list.Items.Count ; i++ )
+            {
+
+                if ( selected_files[i] == null )
+                    break;
+
+                if ( selected_files[i].Contains("2005") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2005;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2005;
+                }
+
+                if ( selected_files[i].Contains("2008") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2008;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2008;
+                }
+
+                if ( selected_files[i].Contains("2010") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2010;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2010;
+                }
+
+                if ( selected_files[i].Contains("2012") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2012;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2012;
+                }
+
+                if ( selected_files[i].Contains("2013") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2013;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2013;
+                }
+
+                if ( selected_files[i].Contains("2015") )
+                {
+                    Process vcinstaller = new Process();
+                    vcinstaller.StartInfo.FileName = selected_files[i];
+                    vcinstaller.StartInfo.Arguments = arguments2015;
+                    vcinstaller.Start();
+                    vcinstaller.WaitForExit();
+
+                    log += "Installed " + selected_files[i] + " with argument " + arguments2015;
+                }
+
+            }
+
+            File.WriteAllText("log_" + DateTime.Today.Ticks + ".txt", log);
+
+            notifyIcon1.Text = "Process is finished";
+            notifyIcon1.BalloonTipText = "Process is done, check in Control Panel or Setting for all the installed packages";
+            notifyIcon1.ShowBalloonTip(1000);
+                
+        }
+
+        private void notifyIcon1_MouseDown(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+        }
+    }
+}
